68b3d8a5462d1e6a1b224a3f2ee740b0ea9efc67
yahyathegreat <yahyamardan1234@gmail.com>
2025-01-03 20:05:57 +0500

    man up lil bro

--- main.py
+++ main.py
@@ -11,7 +11,29 @@ class test_class(ABsclass):
 test_obj = test_class()
 test_obj.task()
 test_obj.print(100)
-
-
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    def move(self):
+        pass
+class Human(Animal):
+    def move(self):
+            print("i can walk and run")
+class Snake(Animal):
+    def move(self):
+         print("i can crawl")
+class Dog(Animal):
+            def move(self):
+                print("i can bark")
+class Lion(Animal):
+            def move(self):
+                print("i can roar")
+R = Human()
+R.move()
+K = Snake()
+K.move()
+R = Dog()
+R.move()
+K = Lion()
+K.move()
 
         
